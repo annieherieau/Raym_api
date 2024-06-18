58baebcb911decc35e15bd3436449cf379965ef4
annieherieau <annie.herieau@gmail.com>
2024-06-18 14:34:57 +0200

    fix cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,10 +2,9 @@ class CartItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_cart
 
+  # POST /cart_items
   def create
     @product = Product.find_by(id: params[:product_id].to_i)
-    puts ("params[:product_id]")
-    puts (params[:product_id])
     if @product
       @cart_item = @cart.cart_items.new(cart_item_params.merge(product: @product))
       if @cart_item.save
@@ -18,7 +17,7 @@ class CartItemsController < ApplicationController
     end
   end
   
-
+  # PUT ou PATCH /cart_items/:id
   def update
     cart_item = @cart.cart_items.find(params[:id])
     if cart_item.update(cart_item_params)
@@ -28,6 +27,7 @@ class CartItemsController < ApplicationController
     end
   end
 
+  # DELETE /cart_items/:id
   def destroy
     cart_item = @cart.cart_items.find(params[:id])
     cart_item.destroy
