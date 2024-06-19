b39755f8e1a8ec90be63e0e96b5d3d6d2f885141
Korblen <malo.bastianelli@gmail.com>
2024-06-19 15:58:20 +0200

    update model product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
     if (!is_admin?)
       render json: {
         status: { code: 401,
-                  message: "Must be an Admin user lol." }
+                  message: "Must be an Admin user." }
       }, status: :unauthorized
     end
   end
