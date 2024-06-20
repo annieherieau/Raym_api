22dbb8f1ec1b0f1e61eff14ecc645c4b07f0c90a
Korblen <malo.bastianelli@gmail.com>
2024-06-21 00:01:05 +0200

    admin routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, only: [:current]
+    before_action :authenticate_user!, only: [:current, :check_admin]
   
     def current
       render json: current_user, only: [:id, :email]
+    end
+
+    def check_admin
+      render json: current_user.admin?
     end
   end
