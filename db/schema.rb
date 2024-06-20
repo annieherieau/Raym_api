e602b97e97d814d9e6d292cc2481056e748958b7
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-06-20 18:17:43 +0200

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_080229) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_103131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
