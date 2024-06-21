4b339a8eaf6705b00a806fd3486561218db00d04
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-06-21 12:58:11 +0200

    some change in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_21_095851) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_21_103709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,6 +104,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_21_095851) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "paid"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
