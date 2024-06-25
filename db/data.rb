2d9b6f7eb919eaac12b08ab0a2ad5f4ba895978c
annieherieau <annie.herieau@gmail.com>
2024-06-25 11:18:07 +0200

    fichier data

---
+++ db/data.rb
@@ -0,0 +1,14 @@
+COLORS = [
+  {name: 'Beige', collection: 'Yann', description: ''},
+  {name: 'Jaune', collection: 'Malo', description: ''},
+  {name: 'Rose', collection: 'Annie', description: ''},
+  {name: 'Gris', collection: 'Robena', description: ''}
+]
+CATEGORIES = [
+  {name: 'On Road', description: ''},
+  {name: 'Off Road',  description: ''},
+  {name: 'Hybrid', description: ''},
+  {name: 'Vêtements Femme',  description: ''},
+  {name: 'Vêtements Homme',  description: ''},
+  {name: 'Accessoires', description: ''}
+]
